909f83974f645093a137a5cb271bbe9cfa02e002
Sarun Tapee <tpzrun@gmail.com>
2022-07-08 10:11:57 +0700

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import "./bootstrap-paths"
+import "dotenv/config"
 import path from "path"
 import { MongoClient } from "mongodb"
 import { HttpServer, HttpServerConfig, HttpServerEvent } from "@server/http"
@@ -14,20 +15,20 @@ import { ErrorServiceConsole } from "@service/error"
   const videoRepo = new VideoRepoFile(videoRepoFileConfig)
 
   const client = await MongoClient.connect(
-    "mongodb://localhost:27017/khampee",
+    process.env.MONGO_URI || "mongodb://localhost:27017",
   ).catch((e) => {
     console.log("fail to connect mongodb:", e)
     process.exit(1)
   })
-  const db = client.db("khampee")
+  const db = client.db(process.env.MONGO_DB_NAME || "testing")
 
   const videoDetailRepo = new VideoDetailRepoMongo(db)
   const videoService = new VideoService(videoRepo, videoDetailRepo)
 
   const httpServerConfig: HttpServerConfig = {
-    port: "8080",
+    port: process.env.PORT || "3000",
     tmpFileDir: path.join(__dirname, "../files/tmp"),
-    domain: "http://localhost:8080",
+    domain: process.env.DOMAIN || "http://localhost:3000",
   }
   const errorService = new ErrorServiceConsole()
 
