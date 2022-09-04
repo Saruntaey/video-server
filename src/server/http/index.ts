c169b63218fc34ba60df951376b4c66a9dba4b0d
Sarun Tapee <tpzrun@gmail.com>
2022-09-04 08:22:32 +0700

    remove tailwind

--- src/server/http/index.ts
+++ src/server/http/index.ts
@@ -60,7 +60,6 @@ export class HttpServer extends EventEmiter {
           <meta charset="UTF-8" />
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <script src="https://cdn.tailwindcss.com"></script>
           <link href="https://vjs.zencdn.net/7.19.2/video-js.css" rel="stylesheet" />
           <link href="/css/videojs-resolution-switcher.css" rel="stylesheet" />
           <title>Document</title>
